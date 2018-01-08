d0bba0113c5de678db2cc4a88c0fa19e380acc1f
mcarthurgill <mcarthur.gill@gmail.com>
2018-01-07 21:47:57 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# week-7-B
+# Arrays
+
+## In `index.html`
+* Open `index.html` in Sublime
+* Open `index.html` in Chrome
+* Open the developer tools console in Chrome.
+
+
+## In `script.js`
+### Look at the array, then use the directions on the index.html file you opened in Chrome. 
---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+  var longAFArray = [11, 52, 1, 53, 22, 26, 2, 44, 2, 23, 17, 87, 102, 68, 434, 634, 53, 113, 3822, 57, 27, 58, 3333, 123123123123123, 40];
+  var longAFString = "supercalifragilisticexpialidocious";
+  // Write your JavaScript below to gather the information needed to complete the sentences in the HTML.
+  
+
+
+
+
+
+
+})
