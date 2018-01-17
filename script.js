eee5265e0758819e1ecfb5dbe3d5c89eefca3557
Your Name <cbetack@gmail.com>
2018-01-16 19:39:31 -0600

    JSchanges

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var longAFArray = [11, 52, 1, 53, 22, 26, 2, 44, 2, 23, 17, 87, 102, 68, 434, 634, 53, 113, 3822, 57, 27, 58, 3333, 123123123123123, 40];
   var longAFString = "supercalifragilisticexpialidocious";
   // Write your JavaScript below to gather the information needed to complete the sentences in the HTML.
-  
-
-
-
-
-
-
+// console.log(longAFArray.length);
+// console.log(longAFArray[0]);
+// console.log(longAFArray[18]);
+// console.log(longAFArray[11]);
+// console.log(longAFArray[15]);
+// console.log(longAFArray.indexOf(3333))
+// console.log(longAFString[0]);
+// console.log(longAFString[28]);
+// console.log(longAFString[11]);
+// console.log(longAFString[19]);
+console.log(longAFString.indexOf("expi"));
 })
